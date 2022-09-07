28da251baa19a69c22d9c3cedf19379ff35420e5
Technotech <technotech2121@gmail.com>
2022-09-07 12:08:36 +1000

    Before zoom change

--- physics.js
+++ physics.js
@@ -60,6 +60,15 @@ class rgb {
     }
 }
 
+class object2D {
+    constructor(vector, velocity, mass, radius) {
+        this.pos = vector
+        this.velocity = velocity
+        this.mass = mass
+        this.radius = radius
+    }
+}
+
 let colorPalette = [
     new rgb(51, 153, 255),
     new rgb(0, 100, 210),
@@ -68,37 +77,33 @@ let colorPalette = [
     new rgb(255, 170, 20),
 ]
 
-let pos = {
-    0: new vector2D(0, 0),
-    1: new vector2D(0, 800),
-    2: new vector2D(0, 2500),
-    3: new vector2D(0, 1000),
-    4: new vector2D(0, 700),
-    5: new vector2D(500, 1800),
-    6: new vector2D(400, 1700),
-    7: new vector2D(2000, 1700),
-    8: new vector2D(2000, 2400),
+let initial = {
+    0: new object2D(new vector2D(0, 0), [0, 0], 1000, 25),
+    1: new object2D(new vector2D(0, 800), [30, 0], 1, 10),
+    2: new object2D(new vector2D(0, 2500), [30, 0], 1, 5),
+    3: new object2D(new vector2D(0, 1500), [30, 0], 1, 5),
+    4: new object2D(new vector2D(0, 3500), [30, 0], 1, 5),
+    5: new object2D(new vector2D(0, 5500), [30, 0], 300, 20),
 }
-let velocity = [
-    [0, 0], 
-    [40, 0], 
-    [20, 0], 
-    [20, 0], 
-    [20, 0], 
-    [40, 0], 
-    [40, 0], 
-    [20, -40],
-    [28, -40],
-] // x, y
-let masses = [1000, 1, 10, 1, 1, 2, 2, 200, 2]
-let rad = [25, 10, 5, 5, 5, 5, 4, 20, 7]
 
-let locations = [[], [], [], [], [], [], [], [], []]
+let pos = []
+let velocity = [] // x, y
+let masses = []
+let rad = []
 
+let locations = []
+let screenLocations = []
 let colors = []
 
-let index = 0;
+for (let i = 0; i < Object.keys(initial).length; i++) {
+    pos.push(initial[i].pos)
+    velocity.push(initial[i].velocity)
+    masses.push(initial[i].mass)
+    rad.push(initial[i].radius)
+    console.log(initial[i])
+}
 
+let index = 0;
 
 let follow = false
 function track() {
@@ -125,12 +130,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     objects = []
     // console.log(pos[0])
-    for (let i = 0; i < Object.keys(pos).length; i++) {
+    for (let i = 0; i < pos.length; i++) {
         colors.push(
             colorPalette[Math.floor(Math.random() * colorPalette.length)]
         )
     }
-    for (let i = 0; i < Object.keys(pos).length; i++) {
+    for (let i = 0; i < pos.length; i++) {
         objects.push(new drawCircle(
             ctx,
             pos[i].x, pos[i].y,
@@ -138,6 +143,8 @@ document.addEventListener("DOMContentLoaded", function () {
             zoom,
             `rgba(${colors[i].r}, ${colors[i].g}, ${colors[i].b}, 1)`
         ))
+        locations.push([])
+        screenLocations.push([])
 
     }
     // console.log(objects)
@@ -179,10 +186,13 @@ document.addEventListener("DOMContentLoaded", function () {
     resize()
     window.addEventListener('resize', resize)
     // console.log(canvas)
-
     $(document).bind('mousewheel', function (evt) {
         let delta = evt.originalEvent.wheelDelta
         zoom += delta
+        finalX += delta * ((zoom * zoomScale))
+        //- (canvas.width / 2) * (zoom * zoomScale)
+        console.log(finalX)
+        // console.log(zoom*zoomScale)
         if (zoom <= 2) {
             zoom = 2
         }
@@ -200,6 +210,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     $(canvas).bind("mousemove", function (e) {
+        console.log(finalX)
         if (down == true) {
             dragX = e.pageX
             dragY = e.pageY;
@@ -239,12 +250,23 @@ document.addEventListener("DOMContentLoaded", function () {
     let flag = false;
     let tweenIndex = 0;
 
+    let lastFollow = false
+    if (lastFollow != follow) {
+        screenLocations = []
+        for (let i = 0; i < pos.length; i++) {
+            screenLocations.push([])
+    
+        }
+    }
+
     ; (() => {
         function main(currentTime) {
             if (follow == true) {
                 // console.log(objects.length)
+                console.log(finalX)
                 if (!index || !lastIndex || index >= objects.length || index < 0) {
                     finalX = -(pos[lastIndex].x * (zoom * zoomScale))
+
                     finalY = (pos[lastIndex].y * (zoom * zoomScale))
                 } else {
                     if (index != lastIndex) {
@@ -271,7 +293,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             if (!index || index > objects.length || index < 0) {
                 lastIndex = lastIndex
-                
+
             } else {
                 lastIndex = index
             }
@@ -292,29 +314,43 @@ document.addEventListener("DOMContentLoaded", function () {
                         // 6.67430 * (10 ** 0)
                         let forceX = ((1) * masses[k] * 1) / (distanceCombined ** 2) * -distance[0]
                         let forceY = ((1) * masses[k] * 1) / (distanceCombined ** 2) * -distance[1]
-                        
+
                         velocity[i] = [velocity[i][0] + (forceX), velocity[i][1] + forceY]
                     }
                 }
-                
+
                 // console.table({ "distance": distance, "forceX": forceX, "forceY": forceY, "angle": angle, "vx": (forceX/oMass)*elapsedSinceLastLoop, "pos": pos })
                 let newX = (canvas.width / 2) + finalX + (pos[i].x * (zoom * zoomScale) + velocity[i][0] * (zoom * zoomScale))
                 let newY = (canvas.height / 2) + finalY - (pos[i].y * (zoom * zoomScale) + velocity[i][1] * (zoom * zoomScale))
                 pos[i] = new vector2D(pos[i].x + velocity[i][0], pos[i].y + velocity[i][1])
-                
+
                 objects[i].update(newX, newY, zoom)
+                // Location update
                 locations[i].push(new vector2D(pos[i].x, pos[i].y))
                 if (locations[i].length == 100) {
                     locations[i].shift()
                 }
-                if (locations[i].length >= 2) {
+
+                // Screen location update
+                let sL = 200
+                if (follow == true) {
+                    lastFollow = true
+                    screenLocations[i].push(new vector2D(newX, newY))
+                    if (screenLocations[i].length == sL) {
+                        screenLocations[i].shift()
+                    }
+                } else {
+                    lastFollow = false
+                }
+
+                if (locations[i].length >= 2 && follow == false) {
                     // ctx.beginPath();
                     // ctx.lineWidth = "2";
                     ctx.strokeStyle = `rgba(${colors[i].r}, ${colors[i].g}, ${colors[i].b}, 1)`;
 
                     for (let m = 1; m < locations[i].length; m++) {
                         ctx.beginPath();
-                        ctx.lineWidth = `${40 * (0.01 * m) * (zoom * zoomScale)}`;
+                        ctx.lineWidth = `${20 * (0.01 * m) * (zoom * zoomScale)}`;
                         ctx.moveTo((canvas.width / 2) + finalX + locations[i][m - 1].x * (zoom * zoomScale), (canvas.height / 2) + finalY - locations[i][m - 1].y * (zoom * zoomScale));
                         ctx.strokeStyle = `rgba(${colors[i].r}, ${colors[i].g}, ${colors[i].b}, ${(0.01 * m)})`;
                         ctx.lineTo((canvas.width / 2) + finalX + locations[i][m].x * (zoom * zoomScale), (canvas.height / 2) + finalY - locations[i][m].y * (zoom * zoomScale));
@@ -333,7 +369,19 @@ document.addEventListener("DOMContentLoaded", function () {
                     //     ctx.fill()
                     // }
 
+                }
 
+                if (screenLocations[i].length >= 2 && follow == true) {
+                    ctx.strokeStyle = `rgba(${colors[i].r}, ${colors[i].g}, ${colors[i].b}, 1)`;
+                    for (let m = 1; m < screenLocations[i].length; m++) {
+                        ctx.beginPath();
+                        ctx.lineWidth = `${40 * (1 / sL * m) * (zoom * zoomScale)}`;
+                        ctx.moveTo(screenLocations[i][m - 1].x, screenLocations[i][m - 1].y);
+                        ctx.strokeStyle = `rgba(${colors[i].r}, ${colors[i].g}, ${colors[i].b}, ${(1 / sL * m)})`;
+                        ctx.lineTo(screenLocations[i][m].x, screenLocations[i][m].y);
+                        ctx.stroke();
+                        ctx.closePath();
+                    }
                 }
             }
             window.requestAnimationFrame(main);
